3fc4db2e49b0ff6ea834cee2db9bf518bb39bc9b
Papryk <gustaw5022@gmail.com>
2020-06-11 21:41:33 +0200

    added fragments

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.tictactoe
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tictactoe.fragments.GameFragment
 import com.example.tictactoe.fragments.MainMenuFragment
+import com.example.tictactoe.fragments.RulesFragment
 
 class MainActivity : AppCompatActivity(),
-        MainMenuFragment.OnFragmentInteractionListener {
+    MainMenuFragment.OnFragmentInteractionListener,
+    GameFragment.OnFragmentInteractionListener,
+    RulesFragment.OnFragmentInteractionListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
